87fe4fa9509aefc3d782d85a488bfbb6efd521b9
Md.Abdullah al mamun <mamun1214@gmail.com>
2023-02-06 19:56:01 +0600

    Disable scroll on mobile menu show

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,12 +1,16 @@
 const hamburger = document.querySelector('.hamburger');
 const navMenu = document.querySelector('.nav-menu');
+const html = document.querySelector('html');
+const body = document.querySelector('body');
 
 hamburger.addEventListener('click', () => {
-  hamburger.classList.toggle('active');
-  navMenu.classList.toggle('active');
+    hamburger.classList.toggle('active');
+    navMenu.classList.toggle('active');
+    body.classList.toggle('disable-scroll');
+    html.classList.toggle('disable-scroll');
 });
 
 document.querySelectorAll('.nav-link').forEach((n) => n.addEventListener('click', () => {
-  hamburger.classList.remove('active');
-  navMenu.classList.remove('active');
+    hamburger.classList.remove('active');
+    navMenu.classList.remove('active');
 }));
