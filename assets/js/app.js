2ecc66a134003a0f8e533f1ca95bd66ece83bf99
Md.Abdullah al mamun <mamun1214@gmail.com>
2023-02-07 23:05:51 +0600

    Make project list dynamic

--- assets/js/app.js
+++ assets/js/app.js
@@ -21,7 +21,7 @@ const projects = [
         }
     },
     {
-        "name": "Multi-Post Stories Gain",
+        "name": "Multi-Post Stories Gain + Glorys",
         "description": "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it 1960s with the releaLorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it 1960s with the relea",
         "image": {
             "featured": "SnapshootPortfolio.svg"
@@ -38,7 +38,58 @@ const projects = [
         }
     },
     {
-        "name": "Multi-Post Stories",
+        "name": "Multi-Post Stories Gain + Glorys",
+        "description": "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it 1960s with the releaLorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it 1960s with the relea",
+        "image": {
+            "featured": "SnapshootPortfolio.svg"
+        },
+        "technologies": [
+            "Ruby on Rails",
+            "Html",
+            "css",
+            "javascript"
+        ],
+        "link": {
+            "live": "#",
+            "source": "#"
+        }
+    },
+    {
+        "name": "Multi-Post Stories Gain + Glorys",
+        "description": "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it 1960s with the releaLorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it 1960s with the relea",
+        "image": {
+            "featured": "SnapshootPortfolio.svg"
+        },
+        "technologies": [
+            "Ruby on Rails",
+            "Html",
+            "css",
+            "javascript"
+        ],
+        "link": {
+            "live": "#",
+            "source": "#"
+        }
+    },
+    {
+        "name": "Multi-Post Stories Gain + Glorys",
+        "description": "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it 1960s with the releaLorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it 1960s with the relea",
+        "image": {
+            "featured": "SnapshootPortfolio.svg"
+        },
+        "technologies": [
+            "Ruby on Rails",
+            "Html",
+            "css",
+            "javascript"
+        ],
+        "link": {
+            "live": "#",
+            "source": "#"
+        }
+    },
+    {
+        "name": "Multi-Post Stories Gain + Glorys",
         "description": "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it 1960s with the releaLorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it 1960s with the relea",
         "image": {
             "featured": "SnapshootPortfolio.svg"
@@ -56,16 +107,69 @@ const projects = [
     },
 ];
 
-hamburger.addEventListener('click', () => {
-    hamburger.classList.toggle('active');
-    navMenu.classList.toggle('active');
-    body.classList.toggle('disable-scroll');
-    html.classList.toggle('disable-scroll');
-});
+document.onreadystatechange = () => {
+    if (document.readyState === 'complete') {
+        hamburger.addEventListener('click', () => {
+            hamburger.classList.toggle('active');
+            navMenu.classList.toggle('active');
+            body.classList.toggle('disable-scroll');
+            html.classList.toggle('disable-scroll');
+        });
 
-document.querySelectorAll('.nav-link').forEach((n) => n.addEventListener('click', () => {
-    hamburger.classList.remove('active');
-    navMenu.classList.remove('active');
-}));
+        document.querySelectorAll('.nav-link').forEach((n) => n.addEventListener('click', () => {
+            hamburger.classList.remove('active');
+            navMenu.classList.remove('active');
+        }));
+        
+        //Recent work project list
+        projects.forEach(function (item) {
+            //console.log(item);
+            let html = "";
+            const parent = document.getElementById("items");
 
+            const article = document.createElement('article');
+            article.classList.add("c-posts__item", "card-wrapper");
 
+            const cardImage = document.createElement('div');
+            cardImage.id = "card-id";
+            cardImage.classList.add("card-image-wrapper");
+
+            const cardContent = document.createElement('div');
+            cardContent.classList.add("card-content-wrapper");
+
+            cardContent.append(
+                Object.assign(
+                    document.createElement('h2'),
+                    {className: 'work-card-title', innerText: item.name}
+                ),
+                Object.assign(
+                    document.createElement('div'),
+                    {id: 'tech-content', className: 'tech-tags'}
+                ),
+                Object.assign(
+                    document.createElement('button'),
+                    {id: '', type: 'button', className: 'c-btn', innerText: 'See Project'}
+                )
+            );
+
+            const ul = document.createElement('ul');
+            item.technologies.forEach(function (tech) {
+                const li = Object.assign(
+                    document.createElement('li'),
+                    {innerText: tech}
+                );
+                /*const a = Object.assign(
+                    document.createElement('a'),
+                    {title: tech, href: '#', rel: 'noopener noreferrer', innerText: tech}
+                );
+                li.append(a);*/
+                ul.append(li);
+            });
+            cardContent.childNodes[1].appendChild(ul);
+
+            article.append(cardImage, cardContent);
+            parent.appendChild(article);
+            console.log(item);
+        });
+    }
+};
